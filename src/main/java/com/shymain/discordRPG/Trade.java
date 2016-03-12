567c6d531512c252b41b5ff01831c5b6169d89e8
Shymain <Shymaintheauthor@gmail.com>
2016-03-11 22:55:06 -0600

    Finished trading functions. Now for the commands.

--- src/main/java/com/shymain/discordRPG/Trade.java
+++ src/main/java/com/shymain/discordRPG/Trade.java
@@ -61,6 +61,10 @@ public class Trade {
 		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
 		JSONObject trades = json.getJSONObject("trades");
 		JSONObject trade = trades.getJSONObject(user.getID());
+		if(trade.getBoolean("trade_close"))
+		{
+			channel.sendMessage("Please either .trade confirm or .trade cancel!");
+		}
 		if(trade.getBoolean("trade_accepted"))
 		{
 			channel.sendMessage("You have already accepted the trade!");
@@ -85,6 +89,11 @@ public class Trade {
 		IUser user = channel.getGuild().getUserByID(openTrades.get(recipient.getID()));
 		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
 		JSONObject trades = json.getJSONObject("trades");
+		JSONObject trade = trades.getJSONObject(user.getID());
+		if(trade.getBoolean("trade_close"))
+		{
+			channel.sendMessage("Please either .trade confirm or .trade cancel!");
+		}
 		trades.remove(user.getID());
 		openTrades.remove(recipient.getID());
 		FileWriter r = new FileWriter(file);
@@ -111,6 +120,10 @@ public class Trade {
 		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
 		JSONObject trades = json.getJSONObject("trades");
 		JSONObject trade = trades.getJSONObject(id);
+		if(trade.getBoolean("trade_close"))
+		{
+			channel.sendMessage("Please either .trade confirm or .trade cancel!");
+		}
 		if(!trades.getJSONObject(id).getBoolean("trade_accepted"))
 		{
 			channel.sendMessage("This trade has not been accepted by both parties!");
@@ -177,6 +190,10 @@ public class Trade {
 		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
 		JSONObject trades = json.getJSONObject("trades");
 		JSONObject trade = trades.getJSONObject(id);
+		if(trade.getBoolean("trade_close"))
+		{
+			channel.sendMessage("Please either .trade confirm or .trade cancel!");
+		}
 		if(!trades.getJSONObject(id).getBoolean("trade_accepted"))
 		{
 			channel.sendMessage("This trade has not been accepted by both parties!");
@@ -262,5 +279,89 @@ public class Trade {
 		r.flush();
 		r.close();
 		channel.sendMessage("Trade cancelled.");
+	}
+	
+	public static void confirm(IChannel channel, IUser user) throws MissingPermissionsException, HTTP429Exception, DiscordException, JSONException, IOException
+	{
+		if(!openTrades.containsKey(user.getID()) && !openTrades.containsValue(user.getID()))
+		{
+			channel.sendMessage("You are not in a trade!");
+			return;
+		}
+		String id = user.getID();
+		IUser user1;
+		IUser recipient;
+		if(openTrades.containsKey(user.getID()))
+		{
+			id = openTrades.get(user.getID());
+		}
+		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
+		JSONObject trades = json.getJSONObject("trades");
+		JSONObject trade = trades.getJSONObject(id);
+		user1 = channel.getGuild().getUserByID(id);
+		recipient = channel.getGuild().getUserByID(trade.getString("recipient"));
+		if(!trade.getBoolean("trade_close"))
+		{
+			trade.remove("trade_close");
+			trade.put("trade_close", true);
+			trade.remove("first_close");
+			trade.put("first_close", user.getID());
+		}else if(trade.getString("first_close").equalsIgnoreCase(user.getID())){
+			channel.sendMessage("You have already confirmed.");
+		}
+		else
+		{
+			Iterator<?> keys = trade.getJSONObject("user1_inventory").keys();
+			while(keys.hasNext())
+			{
+				String key = (String)keys.next();
+				int number = trade.getJSONObject("user1_inventory").getInt(key);
+				Player.inventoryAdd(recipient, key, number);
+			}
+			Iterator<?> keys2 = trade.getJSONObject("user2_inventory").keys();
+			while(keys2.hasNext())
+			{
+				String key = (String)keys.next();
+				int number = trade.getJSONObject("user2_inventory").getInt(key);
+				Player.inventoryAdd(user1, key, number);
+			}
+			trades.remove(id);
+			FileWriter r = new FileWriter(file);
+			r.write(json.toString(3));
+			r.flush();
+			r.close();
+			channel.sendMessage("Trade complete!");
+		}
+	}
+
+	public static void display(IChannel channel, IUser user) throws MissingPermissionsException, HTTP429Exception, DiscordException, JSONException, IOException
+	{
+		JSONObject json = new JSONObject(DiscordRPG.readFile(file));
+		JSONObject json2 = new JSONObject(DiscordRPG.readFile(Item.file));
+		JSONObject trade = json.getJSONObject("trades").getJSONObject(user.getID());
+		JSONObject items = json2.getJSONObject("items");
+		Iterator<?> keys = trade.getJSONObject("user1_inventory").keys();
+		String inventory1 = " offers:\n";
+		int value1 = 0;
+		while(keys.hasNext())
+		{
+			String key = (String)keys.next();
+			int number = trade.getJSONObject("user1_inventory").getInt(key);
+			value1 = value1 + (items.getJSONObject(key).getInt("value")*number);
+			inventory1 += key + ": " + number + ".\n";
+		}
+		Iterator<?> keys2 = trade.getJSONObject("user2_inventory").keys();
+		String inventory2 = " offers:\n";
+		int value2 = 0;
+		while(keys2.hasNext())
+		{
+			String key = (String)keys.next();
+			int number = trade.getJSONObject("user2_inventory").getInt(key);
+			value2 = value2 + (items.getJSONObject(key).getInt("value")*number);
+			inventory1 += key + ": " + number + ".\n";
+		}
+		String name1 = trade.getString("user1");
+		String name2 = trade.getString("user2");
+		channel.sendMessage("```\n"+name1+inventory1+"Total Value: "+value1+".\n"+name2+inventory2+"Total Value: "+value2+".\n```");
 	}
 }
